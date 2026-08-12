8bf1f252d828416c6979f0b6658862791196a35d
Midnight <rabianandos2002@gmail.com>
2026-08-12 08:27:36 +0530

    auth added with middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,14 @@
 import express from 'express'
 import "dotenv/config"
+import { auth } from './lib/auth.js';
+import { toNodeHandler } from 'better-auth/node';
 
 const app = express()
 const PORT = process.env.PORT
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+app.use(express.json());
 
 app.get("/", (_,res) => {
     res.send("Hello world")
